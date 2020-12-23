b66a0863fc00ba728ba5872e46ee12765bbae1e4
KennyMichael <72096449+KennyMichael@users.noreply.github.com>
2020-12-23 18:05:24 +0000

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,11 @@
 # testing gitpod
+
+def printBoard():
+    print(' 1 | 2 | 3')
+    print('-----------')
+    print(' 4 | 5 | 6')
+    print('-----------')
+    print(' 7 | 8 | 9')
+
+
+printBoard()
