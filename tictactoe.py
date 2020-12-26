93c0441df8f77642ecb8b1dcfaaff37a1ee5242f
KennyMichael <72096449+KennyMichael@users.noreply.github.com>
2020-12-26 15:00:59 +0000

    Add files via upload
    Testing board layout with list of inputs, and printing in playable format

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,13 @@
-# testing gitpod
-
-def printBoard():
-    print(' 1 | 2 | 3')
-    print('-----------')
-    print(' 4 | 5 | 6')
-    print('-----------')
-    print(' 7 | 8 | 9')
+# board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+board = [' ', 'O', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ]
 
 
-printBoard()
+def printBoard(board):
+    print(' ' + board[1] + ' ┃ ' + board[2] + ' ┃ ' + board[3])
+    print('━━━━━━━━━━━')
+    print(' ' + board[4] + ' ┃ ' + board[5] + ' ┃ ' + board[6])
+    print('━━━━━━━━━━━')
+    print(' ' + board[7] + ' ┃ ' + board[8] + ' ┃ ' + board[9])
+
+
+printBoard(board)
